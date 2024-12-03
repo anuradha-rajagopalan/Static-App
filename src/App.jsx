58470790ee3f8580a358b9d55b2d58fdb40a66a5
anuradha-rajagopalan <anuradharajarr@gmail.com>
2024-12-03 14:27:24 +0530

    Initialise

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome to vite react Development
+          dev environment
         </p>
       </header>
     </div>
